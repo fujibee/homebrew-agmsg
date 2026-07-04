300f0dda96a4e9d5e543d37bd2a4f7fae94ea33a
fujibee <fujibee@gmail.com>
2026-07-03 18:47:25 -0700

    agmsg 0.1.1

--- Casks/agmsg.rb
+++ Casks/agmsg.rb
@@ -1,15 +1,15 @@
 cask "agmsg" do
-  version "0.1.0"
-  sha256 "a3afb4c35fb78c80c84708dcecfa455b9cd9ecb82b93bd1b2a199a346e905577"
+  version "0.1.1"
+  sha256 "c73013122375e3ffbed51b32cdb19b60af09b9b2e7b7a3dc3932ff0ff73c7557"
 
-  url "https://github.com/fujibee/agmsg/releases/download/app-v#{version}/agmsg-app_#{version}_aarch64.dmg"
+  url "https://github.com/fujibee/agmsg/releases/download/app-v#{version}/agmsg_#{version}_aarch64.dmg"
   name "agmsg"
   desc "Agent messaging desktop app"
   homepage "https://agmsg.cc/"
 
   depends_on arch: :arm64
 
-  app "agmsg app.app"
+  app "agmsg.app"
 
   zap trash: [
     "~/Library/Application Support/agmsg",
