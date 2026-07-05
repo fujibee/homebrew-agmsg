c20e52c37936a8db44fb0d0f9c07f01f7072ada7
fujibee <fujibee@gmail.com>
2026-07-04 21:59:26 -0700

    agmsg 0.1.4

--- Casks/agmsg.rb
+++ Casks/agmsg.rb
@@ -1,6 +1,6 @@
 cask "agmsg" do
-  version "0.1.3"
-  sha256 "c8d84d2521a15aee924650fba98b718693ae82398e960b3dc2e3b2fec05b6c8c"
+  version "0.1.4"
+  sha256 "5f092c82b0237786512917bfc5db221b090c9e0bc2de9239bdf02f632257b726"
 
   url "https://github.com/fujibee/agmsg/releases/download/app-v#{version}/agmsg_#{version}_aarch64.dmg"
   name "agmsg"
